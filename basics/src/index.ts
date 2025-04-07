2ae4e23cb2fcaef5a5821d6ea1b0ad77d330efd5
Norbert Spiess <norbert@3ap.ch>
2025-04-07 13:14:29 +0200

    add explanation for openai parameters

--- basics/src/index.ts
+++ basics/src/index.ts
@@ -6,7 +6,20 @@ const openai = new OpenAI()
 async function main(){
     const response = await openai.chat.completions.create({
         model: 'gpt-4o-mini',
+        // 0 to 2 - less to more random
+        temperature: undefined,
+        // how much of token space to use (higher, bigger token pool is used)
+        top_p: undefined,
+        // limit answer to amount of tokens
+        max_tokens: undefined,
+        // how many choices in answer
+        n: 1,
+        // typical seeding, for determinism or not
+        seed: undefined,
         messages:[{
+            //  system - "configuring" role for behaviour
+            //  user - actual user input
+            //  assistant - responses from openai
             role: 'system',
             content: `You respond like a cool bro, and you respond in JSON format, like this:
                 coolnessLevel: 1-10,
@@ -20,6 +33,9 @@ async function main(){
     console.log(response.choices[0].message)
 }
 
+/**
+ * tokenize a text
+ */
 function encodePrompt(){
     const prompt = "How are you today?"
     const encoder = encoding_for_model('gpt-4o-mini');
@@ -27,4 +43,5 @@ function encodePrompt(){
     console.log(words)
 }
 
-main();
+// encodePrompt();
+// main()
